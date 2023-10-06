86febe0fedcb5779a278e91e189ccdafa84f2399
GeluHorotan <horotangelu17@gmail.com>
2023-10-07 01:55:22 +0300

    feat: display navigation items

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="">TEST</div>;
+  return <div className=""></div>;
 }
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,9 +1,29 @@
 import { FC } from 'react';
+import navigationData from '@/locale/ro/navigationData.json';
 
 const Navigation: FC = () => {
+  const { main_buttons, secondary_buttons } = navigationData;
   return (
-    <nav className="h-screen w-[15%] flex items-center justify-center bg-red-700 rounded-[20px]">
-      T
+    <nav className="h-screen w-[15%] px-9 pb-9 pt-6 flex items-center justify-between bg-blue-300 rounded-[20px] flex-col  text-[#fff] text-base ">
+      <div>LOGO</div>
+      <div className="flex flex-col items-start gap-9 w-full">
+        {main_buttons?.map((btn, i) => {
+          return (
+            <p key={i} className="font-medium">
+              {btn?.title}
+            </p>
+          );
+        })}
+      </div>
+      <div className="flex flex-col items-start gap-9 w-full">
+        {secondary_buttons?.map((btn, i) => {
+          return (
+            <p key={i} className="font-medium">
+              {btn?.title}
+            </p>
+          );
+        })}
+      </div>
     </nav>
   );
 };
