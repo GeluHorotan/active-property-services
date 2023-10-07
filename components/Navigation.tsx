13d51c91d4995643fcc1168a717d4b23d74d2b63
GeluHorotan <horotangelu17@gmail.com>
2023-10-07 03:30:38 +0300

    fix: gap issue between nav items

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -7,17 +7,17 @@ import IconFinder from './svgs/IconFinder';
 const Navigation: FC = () => {
   const { main_buttons, secondary_buttons } = navigationData;
   return (
-    <nav className=" w-[15%] px-9 py-6 flex items-center justify-between gap-[190px] bg-blue-300 rounded-[20px] flex-col  text-[#fff] text-base ">
+    <nav className=" w-[15%] px-3 py-6 flex items-center justify-between gap-[190px] bg-blue-300 rounded-[20px] flex-col  text-[#fff] text-base ">
       <div className="flex flex-col items-start gap-16 w-full ">
         <div className="w-full flex">
           <Logo />
         </div>
-        <div className="flex flex-col items-start gap-3 w-full   ">
+        <div className="flex flex-col items-start  w-full    ">
           {main_buttons?.map((btn, i) => {
             return (
               <div
                 key={i}
-                className="rounded-2xl flex gap-5 w-full  px-5 py-4 pointer-events-none hover:bg-[#5F76C7] duration-300 ease-out transition-all"
+                className="rounded-2xl py-4 px-5 flex gap-5 w-full   pointer-events-none hover:bg-[#5F76C7] duration-300 ease-out transition-all"
               >
                 <IconFinder name={btn?.iconName} />
                 <Link
@@ -32,19 +32,19 @@ const Navigation: FC = () => {
           })}
         </div>
       </div>
-      <div className="flex flex-col  items-start gap-9 w-full ">
+      <div className="flex flex-col  items-start  w-full ">
         {secondary_buttons?.map((btn, i) => {
           return (
             <div
               key={i}
-              className="rounded-2xl w-full pointer-events-none text-[#fff]  hover:text-[#5F76C7] flex gap-5 duration-300 ease-out transition-all"
+              className="rounded-2xl py-4 px-5 w-full pointer-events-none text-[#fff]  hover:text-[#5F76C7] flex gap-5 duration-300 ease-out transition-all"
             >
               <IconFinder name={btn?.iconName}></IconFinder>
 
               <Link
                 href={'#'}
                 key={i}
-                className="font-medium px-5 pointer-events-auto"
+                className="font-medium pointer-events-auto"
               >
                 {btn?.title}
               </Link>
