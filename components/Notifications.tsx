8b1878cc69cf4878861221cab3c2d4f885e18bc0
GeluHorotan <horotangelu17@gmail.com>
2023-10-08 02:42:32 +0300

    feat: create body part with json data

--- components/Notifications.tsx
+++ components/Notifications.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react';
+import NotificationData from '@ro/NotificationData.json';
 
 const Notifications: FC = () => {
+  const { notifications } = NotificationData;
   return (
-    <div className="   col-span-7 h-full flex-col flex ">
+    <div className=" row-span-1  col-span-7 h-full flex-col flex  ">
       {/* Header */}
       <div className="flex w-full gap-[5px]">
         <div className="w-[152px] h-[43px] bg-white rounded-t-[15px] flex items-center justify-center">
@@ -19,8 +21,47 @@ const Notifications: FC = () => {
         </div>
       </div>
       {/* Body */}
-      <div className="w-full h-full flex flex-col gap-5 bg-white rounded-b-4xl py-[28px] pl-[22.3px] pr-[30px] ">
+      <div className="w-full h-[479px]  flex flex-col gap-5 bg-white  rounded-b-4xl py-[28px] pl-[22.3px] pr-[30px]  ">
         <h4 className="font-semibold text-semibold">Notificari</h4>
+        <div className="flex flex-col gap-[25px] h-full overflow-y-scroll  ">
+          {notifications?.map((notification, i) => {
+            return (
+              <div key={i} className="flex flex-col gap-[9px]">
+                <div className="flex items-center justify-between pr-5 ">
+                  <div className="gap-[13.5px] flex items-center">
+                    <h5
+                      className={`${
+                        notification.isRead
+                          ? 'font-regular text-gray-800'
+                          : 'font-medium text-gray-900'
+                      }  leading-6`}
+                    >
+                      {notification?.title}
+                    </h5>
+                    <div className="flex gap-[7.5px] items-center">
+                      {notification?.tags.map((tag, i) => {
+                        return (
+                          <div
+                            key={i}
+                            className="py-1 px-[11px]  rounded-4xl flex items-center justify-center bg-red-400"
+                          >
+                            <p className="text-[12px] font-regular">
+                              {tag.name}
+                            </p>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                  <p>{notification.date}</p>
+                </div>
+                <p className="text-[12px] font-regular leading-[18px]">
+                  {notification.desc}
+                </p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
