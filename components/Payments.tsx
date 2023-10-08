a9e9d16ee7b99154027b13807565dbd17f074bbd
GeluHorotan <horotangelu17@gmail.com>
2023-10-08 22:00:46 +0300

    feat: implement donught graph using react chart js

--- components/Payments.tsx
+++ components/Payments.tsx
@@ -1,4 +1,6 @@
 'use client';
+import { Chart as ChartJS, ArcElement, Legend } from 'chart.js';
+import { Doughnut, Pie } from 'react-chartjs-2';
 import { useState, FC } from 'react';
 import PanelHeader from '@components/PanelHeader';
 import PaymentData from '@ro/PaymentData.json';
@@ -7,12 +9,34 @@ const Payments: FC = () => {
   const [view, setView] = useState<string>('plati');
   const { categories, payments } = PaymentData;
 
+  ChartJS.register(ArcElement);
+
+  const data = {
+    labels: ['Mentenanta si reparatii', 'Altele'],
+    datasets: [
+      {
+        data: [100, 500],
+
+        backgroundColor: ['#ECECEC', '#838FDE'],
+        borderWidth: 0,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true, // Allows the chart to be responsive
+    maintainAspectRatio: false, // Prevents maintaining aspect ratio
+    width: 200, // Set the width
+    height: 200, // Set the height
+    cutoutPercentage: 70,
+  };
+
   return (
-    <div className="h-[473px]  col-span-6 ">
+    <div className="  col-span-6 ">
       <PanelHeader categories={categories} setView={setView} view={view} />
 
-      <div className="w-full h-full bg-white rounded-b-4xl rounded-tr-4xl py-[25px] pl-[25px] pr-5 ">
-        <div className="w-full gap-[15px] flex flex-col">
+      <div className="w-full bg-white rounded-b-4xl rounded-tr-4xl pt-[25px] pb-[48px] pl-[25px] pr-5 ">
+        <div className="w-full gap-[15px] flex flex-col   h-full ">
           <div className=" w-full flex items-center justify-between">
             <h4 className="font-semibold leading-[27px]">{payments.title}</h4>
             <div className="flex items-center justify-center gap-[10px]">
@@ -24,19 +48,36 @@ const Payments: FC = () => {
               </div>
             </div>
           </div>{' '}
-          <div className="flex items-center gap-[25px]">
-            {payments.spent_on.map((pay, i) => {
-              return (
-                <div key={i} className="flex gap-2 items-center">
-                  <div
-                    className={`${
-                      pay.is_prioritized ? 'bg-custom_blue-200' : 'bg-[#ECECEC]'
-                    } w-3 h-3 rounded-full`}
-                  ></div>
-                  <div>{pay.title}</div>
-                </div>
-              );
-            })}
+          <div className="flex flex-col gap-[57px]">
+            <div className="flex items-center gap-[25px]  ">
+              {payments.spent_on.map((pay, i) => {
+                return (
+                  <div key={i} className="flex gap-2 items-center">
+                    <div
+                      className={`${
+                        pay.is_majority ? 'bg-custom_blue-200' : 'bg-[#ECECEC]'
+                      } w-3 h-3 rounded-full`}
+                    />
+                    <div className="text-[14px] font-normal leading-[21px]">
+                      {pay.label_title}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="relative flex items-center   w-[240px] h-max ml-5  ">
+              <Doughnut
+                data={data}
+                options={{ cutout: 97, layout: { padding: 0 } }}
+                className=""
+              />
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center flex-col">
+                <h6 className="font-light leading-[23px]">TOTAL</h6>
+                <h2 className="font-semibold leading-[43px]">
+                  &euro;{payments.total_spent}
+                </h2>
+              </div>
+            </div>
           </div>
         </div>
       </div>
