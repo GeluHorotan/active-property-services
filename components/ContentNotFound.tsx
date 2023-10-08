04433fd36a2fd44eb838544611f762a3ecc2bf8e
GeluHorotan <horotangelu17@gmail.com>
2023-10-08 23:05:44 +0300

    feat: handle content not found for received view

--- components/ContentNotFound.tsx
+++ components/ContentNotFound.tsx
@@ -1,11 +1,17 @@
+import { cn } from '@lib/utils';
 import { FC } from 'react';
 
 interface IContentNotFound {
-  children?: React.ReactNode;
+  children: React.ReactNode;
+  className?: string;
 }
 
-const ContentNotFound: FC<IContentNotFound> = ({ children }) => {
-  return <h6 className="font-semibold text-custom_gray-700">{children}</h6>;
+const ContentNotFound: FC<IContentNotFound> = ({ children, className }) => {
+  return (
+    <h6 className={cn('font-semibold text-custom_gray-700', className)}>
+      {children}
+    </h6>
+  );
 };
 
 export default ContentNotFound;
