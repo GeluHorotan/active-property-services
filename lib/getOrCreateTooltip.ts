381bcc7c402f3f17df64d1cef73f1c3e65ca89da
GeluHorotan <horotangelu17@gmail.com>
2023-10-09 22:01:15 +0300

    style: break down the tooltip functions

---
+++ lib/getOrCreateTooltip.ts
@@ -0,0 +1,23 @@
+export const getOrCreateTooltip = (chart: any) => {
+  let tooltipEl = chart.canvas.parentNode.querySelector('div');
+
+  if (!tooltipEl) {
+    tooltipEl = document.createElement('div');
+    tooltipEl.style.background = '#fff';
+    tooltipEl.style.borderRadius = '3px';
+    tooltipEl.style.color = 'black';
+    tooltipEl.style.opacity = 1;
+    tooltipEl.style.pointerEvents = 'none';
+    tooltipEl.style.position = 'absolute';
+    tooltipEl.style.transform = 'translate(-50%, 0)';
+    tooltipEl.style.transition = 'all .1s ease';
+
+    const table = document.createElement('table');
+    table.style.margin = '0px';
+
+    tooltipEl.appendChild(table);
+    chart.canvas.parentNode.appendChild(tooltipEl);
+  }
+
+  return tooltipEl;
+};
