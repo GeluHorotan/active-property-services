f8715e3bd823674740c42f8cf9c9bd906715038c
GeluHorotan <horotangelu17@gmail.com>
2023-10-10 23:54:12 +0300

    fix: add loading state for charts

--- components/PaymentBody.tsx
+++ components/PaymentBody.tsx
@@ -41,89 +41,90 @@ const PaymentBody: FC<IPaymentBody> = ({ payments }) => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-[57px]">
-        <div className="flex items-center gap-[25px]  ">
-          {payments?.spent_on.map((pay, i) => {
-            return (
-              <div key={i} className="flex gap-2 items-center">
-                <div
-                  className={`${
-                    pay.is_majority ? 'bg-custom_blue-200' : 'bg-[#ECECEC]'
-                  } w-3 h-3 rounded-full`}
-                />
-                <div className="text-[14px] font-normal leading-[21px]">
-                  {pay.label_title}
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className="flex w-full justify-between items-end ">
-          <div className="relative flex items-center   w-[240px] min-h-[280px] h-max ml-5  ">
-            <div className=" w-full flex items-center justify-center">
-              {isLoading && <Spinner size="lg" />}
-            </div>
-            {!isLoading && (
-              <>
-                <Doughnut
-                  data={data}
-                  options={{
-                    cutout: 95,
-                    layout: { padding: 0 },
-                  }}
-                  className=""
-                />
-                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center flex-col">
-                  <h6 className="font-light leading-[23px]">TOTAL</h6>
-                  <h2 className="font-semibold leading-[43px]">
-                    &euro;{payments.total_spent}
-                  </h2>
-                </div>
-              </>
-            )}
-          </div>
-          <div className="flex flex-col gap-[18px]">
-            {payments?.table?.map((entry, i) => {
-              return (
-                <div
-                  key={i}
-                  className={`flex ${
-                    entry.sub_title ? 'items-start' : 'items-center'
-                  } gap-[74px] `}
-                >
-                  <div
-                    className={`flex gap-[10px] ${
-                      entry.sub_title ? ' items-start' : 'items-center'
-                    } `}
-                  >
-                    <IconFinder name={entry.iconName} />
-                    <span className="text-[14px] font-normal leading-[21px]">
-                      {entry.title}
-                      {entry.sub_title && (
-                        <>
-                          <br /> {entry.sub_title}
-                        </>
-                      )}
-                    </span>
-                  </div>
 
-                  <h5
-                    className={`w-full font-semibold  text-end leading-[25px] ${
-                      entry?.type === 'paid'
-                        ? 'text-custom_red-700'
-                        : entry?.type === 'received'
-                        ? 'text-[#079348]'
-                        : 'text-custom_gray-900'
-                    }`}
-                  >
-                    &euro; {entry?.amount}
-                  </h5>
+      {isLoading && (
+        <div className="w-full h-[450px] flex items-center justify-center ">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {!isLoading && (
+        <div className="flex flex-col gap-[57px]  ">
+          <div className="flex items-center gap-[25px]  ">
+            {payments?.spent_on.map((pay, i) => {
+              return (
+                <div key={i} className="flex gap-2 items-center">
+                  <div
+                    className={`${
+                      pay.is_majority ? 'bg-custom_blue-200' : 'bg-[#ECECEC]'
+                    } w-3 h-3 rounded-full`}
+                  />
+                  <div className="text-[14px] font-normal leading-[21px]">
+                    {pay.label_title}
+                  </div>
                 </div>
               );
             })}
           </div>
+          <div className="flex w-full justify-between items-end ">
+            <div className="relative flex items-center   w-[240px] min-h-[280px] h-max ml-5  ">
+              <Doughnut
+                data={data}
+                options={{
+                  cutout: 95,
+                  layout: { padding: 0 },
+                }}
+                className=""
+              />
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center flex-col">
+                <h6 className="font-light leading-[23px]">TOTAL</h6>
+                <h2 className="font-semibold leading-[43px]">
+                  &euro;{payments.total_spent}
+                </h2>
+              </div>
+            </div>
+            <div className="flex flex-col gap-[18px]">
+              {payments?.table?.map((entry, i) => {
+                return (
+                  <div
+                    key={i}
+                    className={`flex ${
+                      entry.sub_title ? 'items-start' : 'items-center'
+                    } gap-[74px] `}
+                  >
+                    <div
+                      className={`flex gap-[10px] ${
+                        entry.sub_title ? ' items-start' : 'items-center'
+                      } `}
+                    >
+                      <IconFinder name={entry.iconName} />
+                      <span className="text-[14px] font-normal leading-[21px]">
+                        {entry.title}
+                        {entry.sub_title && (
+                          <>
+                            <br /> {entry.sub_title}
+                          </>
+                        )}
+                      </span>
+                    </div>
+
+                    <h5
+                      className={`w-full font-semibold  text-end leading-[25px] ${
+                        entry?.type === 'paid'
+                          ? 'text-custom_red-700'
+                          : entry?.type === 'received'
+                          ? 'text-[#079348]'
+                          : 'text-custom_gray-900'
+                      }`}
+                    >
+                      &euro; {entry?.amount}
+                    </h5>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- components/PaymentsGraphBody.tsx
+++ components/PaymentsGraphBody.tsx
@@ -1,12 +1,12 @@
 import { Dropdown as IDropdown } from '@/types/IPaymentGraph';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import Button from '@components/Button';
 import IconFinder from '@icons/IconFinder';
 import Dropdown from '@components/Dropdown';
 import { Bar } from 'react-chartjs-2';
 import { barData, barOptions } from '@lib/chartjs/barChartConfig';
 import AnnotationPlugin from 'chartjs-plugin-annotation';
-
+import { Spinner } from '@nextui-org/react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -42,14 +42,18 @@ const PaymentsGraphBody: FC<IPaymentsGraphBody> = ({
   title,
   button,
 }) => {
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    AnnotationPlugin
-  );
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  useEffect(() => {
+    ChartJS.register(
+      CategoryScale,
+      LinearScale,
+      BarElement,
+      AnnotationPlugin,
+      Title,
+      Tooltip
+    );
+    setIsLoading(false);
+  }, []);
   return (
     <div className="w-full flex flex-col gap-4   pb-[18px] ">
       {/* Top part */}
@@ -63,71 +67,81 @@ const PaymentsGraphBody: FC<IPaymentsGraphBody> = ({
         </Button>
       </div>
       {/* Spendings and Dropdown */}
-      <div className="grid grid-cols-13 gap-[19.5px]">
-        <Dropdown title={dropdown?.title}>
-          {dropdown?.entries.map((entry, i) => {
-            return (
-              <Menu.Item key={i}>
-                {({ active }) => (
-                  <div
-                    className={`${
-                      active && 'bg-[#F1EFFD]'
-                    } text-[14px] cursor-pointer text-ellipsis font-normal overflow-hidden whitespace-nowrap leading-[21px] text-custom_gray-900 px-[18px] py-2 `}
-                  >
-                    {entry?.title}
-                  </div>
-                )}
-              </Menu.Item>
-            );
-          })}
-        </Dropdown>
-        <div className=" col-span-9 flex flex-col gap-[10px]">
-          <div className="flex gap-[10px] items-center">
-            {graph_legend?.map(
-              (
-                legend: { title: string; color: string; bg_color: string },
-                i: number
-              ) => {
-                return (
-                  <div key={i} className="flex gap-[8px] items-center">
-                    <div
-                      className={`w-3 h-3 ${legend?.bg_color} rounded-full `}
-                    />
-                    <span
-                      className={`font-normal leading-[21px] text-[${legend?.color}] text-[14px]`}
-                    >
-                      {legend.title}
-                    </span>
-                  </div>
-                );
-              }
-            )}
-          </div>
-          {/* Spendings */}
-          <div className="flex gap-[15px] items-center ">
-            {graph_sub_legend?.map(
-              (
-                sub_legend: {
-                  text: string;
-                  color: string;
-                },
-                i: number
-              ) => {
-                return (
-                  <span
-                    key={i}
-                    className={`${sub_legend?.color} font-medium leading-[21px] text-[14px] `}
-                    dangerouslySetInnerHTML={{ __html: sub_legend?.text }}
-                  />
-                );
-              }
-            )}
-          </div>
+      {isLoading && (
+        <div className="w-full h-[450px] flex items-center justify-center ">
+          <Spinner size="lg" />
         </div>
-      </div>
-      <div className="    w-full  ">
-        <Bar data={barData} options={barOptions} />
-      </div>
+      )}
+      {!isLoading && (
+        <>
+          {' '}
+          <div className="grid grid-cols-13 gap-[19.5px]">
+            <Dropdown title={dropdown?.title}>
+              {dropdown?.entries.map((entry, i) => {
+                return (
+                  <Menu.Item key={i}>
+                    {({ active }) => (
+                      <div
+                        className={`${
+                          active && 'bg-[#F1EFFD]'
+                        } text-[14px] cursor-pointer text-ellipsis font-normal overflow-hidden whitespace-nowrap leading-[21px] text-custom_gray-900 px-[18px] py-2 `}
+                      >
+                        {entry?.title}
+                      </div>
+                    )}
+                  </Menu.Item>
+                );
+              })}
+            </Dropdown>
+            <div className=" col-span-9 flex flex-col gap-[10px]">
+              <div className="flex gap-[10px] items-center">
+                {graph_legend?.map(
+                  (
+                    legend: { title: string; color: string; bg_color: string },
+                    i: number
+                  ) => {
+                    return (
+                      <div key={i} className="flex gap-[8px] items-center">
+                        <div
+                          className={`w-3 h-3 ${legend?.bg_color} rounded-full `}
+                        />
+                        <span
+                          className={`font-normal leading-[21px] text-[${legend?.color}] text-[14px]`}
+                        >
+                          {legend.title}
+                        </span>
+                      </div>
+                    );
+                  }
+                )}
+              </div>
+
+              <div className="flex gap-[15px] items-center ">
+                {graph_sub_legend?.map(
+                  (
+                    sub_legend: {
+                      text: string;
+                      color: string;
+                    },
+                    i: number
+                  ) => {
+                    return (
+                      <span
+                        key={i}
+                        className={`${sub_legend?.color} font-medium leading-[21px] text-[14px] `}
+                        dangerouslySetInnerHTML={{ __html: sub_legend?.text }}
+                      />
+                    );
+                  }
+                )}
+              </div>
+            </div>
+          </div>
+          <div className="    w-full  ">
+            <Bar data={barData} options={barOptions} />
+          </div>{' '}
+        </>
+      )}
     </div>
   );
 };
